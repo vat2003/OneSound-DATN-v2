14bc9e832dc70d729fd266c9cd5b124f78183e23
vat2003 <anhtaivo43@gmail.com>
2024-01-15 00:14:05 +0700

    merge - uyentphuong

--- Font-end/admin-fontend-DATN-0001/src/app/pages/manage/managealbum-admin/managealbum-admin.component.ts
+++ Font-end/admin-fontend-DATN-0001/src/app/pages/manage/managealbum-admin/managealbum-admin.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import {FileService} from "../../../services/file.service";
 
 @Component({
   selector: 'app-managealbum-admin',
   standalone: true,
   imports: [],
   templateUrl: './managealbum-admin.component.html',
-  styleUrl: './managealbum-admin.component.scss'
+  styleUrl: './managealbum-admin.component.scss',
+
 })
 export class ManagealbumAdminComponent {
+  constructor() {
+  }
 
 }
